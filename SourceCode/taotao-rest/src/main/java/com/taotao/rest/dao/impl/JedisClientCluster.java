fcb3613851b4e9bd45d9328c4e9bbf43b0262dd1
liyb <908230189@qq.com>
2018-03-23 23:37:53 +0800

    提交修改

---
+++ SourceCode/taotao-rest/src/main/java/com/taotao/rest/dao/impl/JedisClientCluster.java
@@ -0,0 +1,56 @@
+package com.taotao.rest.dao.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.taotao.rest.dao.JedisClient;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisCluster;
+import redis.clients.jedis.JedisPool;
+/**
+ * 集群版redis
+ * @author liyb
+ *
+ */
+//不添加注解，手动在配置文件配
+public class JedisClientCluster implements JedisClient {
+	
+	@Autowired
+	private JedisCluster jedisCluster;
+	
+	@Override
+	public String get(String key) {
+		return jedisCluster.get(key);
+	}
+
+	@Override
+	public String set(String key, String value) {
+		return jedisCluster.set(key, value);
+	}
+
+	@Override
+	public String hget(String hkey, String key) {
+		return jedisCluster.hget(hkey, key);
+	}
+
+	@Override
+	public long hset(String hkey, String key, String value) {
+		return jedisCluster.hset(hkey, key, value);
+	}
+
+	@Override
+	public long incr(String key) {
+		return jedisCluster.incr(key);
+	}
+
+	@Override
+	public long expire(String key, int second) {
+		return jedisCluster.expire(key, second);
+	}
+
+	@Override
+	public long ttl(String key) {
+		return jedisCluster.ttl(key);
+	}
+
+}
