adb05ac9a30db9dab46f52eab9d0c27cbe4446cd
liyb <908230189@qq.com>
2018-11-13 23:37:01 +0800

    init taozhi

--- TaoZhi/taozhi/src/main/java/com/leekoko/taozhi/TestAPJ/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.leekoko.taozhi.TestAPJ.repository;
-
-import com.leekoko.taozhi.TestAPJ.pojo.Deparment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepartmentRepository extends JpaRepository<Deparment,Long>{
-}
--- TaoZhi/taozhi/src/main/java/com/leekoko/taozhi/TestAPJ/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.leekoko.taozhi.TestAPJ.repository;
-
-import com.leekoko.taozhi.TestAPJ.pojo.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role,Long> {
-}
